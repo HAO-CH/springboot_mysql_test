8227324f671832629244ba96d4995e704354db90
HAO-CH <951921322@qq.com>
2020-03-23 22:47:21 +0800

    commit test2

--- src/main/java/com/dk/proxy/ProxyTest.java
+++ src/main/java/com/dk/proxy/ProxyTest.java
@@ -8,7 +8,7 @@ public class ProxyTest {
         //真实实现
         RealMovie realmovie = new RealMovie();
 
-        //*****************动态代理方式
+        //动态代理方式
         DynamicCinemaProxy  cinemaInvocation = new DynamicCinemaProxy(realmovie);
         //loader 自然是类加载器;interfaces 代码要用来代理的接口;  h 一个 InvocationHandler 对象
         Movie movie = (Movie) Proxy.newProxyInstance(Movie.class.getClassLoader(),new Class[]{Movie.class},cinemaInvocation);
